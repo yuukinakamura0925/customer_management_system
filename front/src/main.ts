048f35e7396b3b3deba58df3b1efd648a328bedc
yuukinakamura0925 <72909522+yuukinakamura0925@users.noreply.github.com>
2021-05-04 21:50:35 +0900

    Add: customer.vue (#6)

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import axios from "axios";
+import router from "./router/index";
 Vue.config.productionTip = false;
 
+Vue.use(axios);
+
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
