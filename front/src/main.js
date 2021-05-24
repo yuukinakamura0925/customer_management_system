19bba56ce6c486297aa89b451a3f106b702951d7
yuukinakamura0925 <ynakamara04@gmail.com>
2021-05-25 00:59:12 +0900

    Add setup

--- front/src/main.js
+++ front/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import router from "./router/index";
+import router from "./router";
 import vuetify from "./plugins/vuetify";
 Vue.config.productionTip = false;
 
@@ -10,5 +10,6 @@ Vue.use(VueAxios, axios);
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
