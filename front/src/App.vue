786d4d3a289b5e1a7c5b25558bf40046b09fdbed
yuukinakamura0925 <ynakamara04@gmail.com>
2021-07-21 01:15:14 +0900

    front src App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,13 +4,26 @@
       <Header/>
       <SideNav/>
       <router-view/>
+      
+      <v-container>
+        <div v-if="this.$router.currentRoute.name === 'home'">
+        </div>
+        <div v-else-if="this.$router.currentRoute.name === 'login'">
+        </div>
+        <div v-else>
+          <v-btn class="mt-16" color="primary lighten-1" @click="$router.push({ name: 'home'})">HOMEに戻る</v-btn>
+        </div>
+      </v-container>
+      
       <!-- <Footer/> -->
     </v-app>
   </div>
 </template>
 
 <script lang="js">
+import firebase from 'firebase'
 import Vue from "vue";
+import { mapActions } from 'vuex'
 // import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
@@ -23,8 +36,24 @@ export default Vue.extend({
   },
   data(){
     return {
-        
     }
+  },
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+        if (this.$router.currentRoute.name === 'login') {
+        this.$router.push({ name: 'home' }, () => {})
+        }
+      } else {
+        this.deleteLoginUser()
+        this.$router.push({ name: 'login' }, () => {})
+      }
+    })
+    
+  },
+  methods: {
+    ...mapActions(['setLoginUser', 'logout', 'deleteLoginUser'])
   }
 });
 </script>
@@ -38,4 +67,9 @@ export default Vue.extend({
   color: #2c3e50;
   margin-top: 60px;
 }
+.serch_box {
+  width: 400px !important;
+  margin:0 auto;
+}
 </style>
+
