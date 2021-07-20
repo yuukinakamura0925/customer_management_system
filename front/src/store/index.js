feca3efc3a2c408dcfd04c9395e4f61cbd8b6e25
yuukinakamura0925 <ynakamara04@gmail.com>
2021-07-21 01:14:37 +0900

    front src store

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,20 +1,46 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import firebase from "firebase";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    login_user: null,
     drawer: false,
   },
   mutations: {
+    setLoginUser(state, user) {
+      state.login_user = user;
+    },
+    deleteLoginUser(state) {
+      state.login_user = null;
+    },
     toggleSideMenu(state) {
       state.drawer = !state.drawer;
     },
   },
   actions: {
+    setLoginUser({ commit }, user) {
+      commit("setLoginUser", user);
+    },
+    deleteLoginUser({ commit }) {
+      commit("deleteLoginUser");
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    login() {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(google_auth_provider);
+    },
     toggleSideMenu({ commit }) {
       commit("toggleSideMenu");
     },
+  },
+  getters: {
+    // stateにログインユーザーがいたら表示、いなかったら空をいれる。（エラーにならないように空の値を入れる）
+    userName: (state) => (state.login_user ? state.login_user.displayName : ""),
+    photoURL: (state) => (state.login_user ? state.login_user.photoURL : ""),
   },
 });
