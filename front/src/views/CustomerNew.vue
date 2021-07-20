4bf70ddd0303da733c72794e6cb6127d123ee1c2
yuukinakamura0925 <ynakamara04@gmail.com>
2021-07-21 00:36:08 +0900

    CustomerNew.vue

--- front/src/views/CustomerNew.vue
+++ front/src/views/CustomerNew.vue
@@ -3,17 +3,17 @@
     <v-container text-xs-center>
       <v-layout row wrap justify-center>
         <v-flex xs12 class="text-center">
-          <h1>New customer form</h1>
+          <h1>顧客登録</h1>
         </v-flex>
 
-        <v-flex xs5 mt-5>
+        <v-flex sm3 mt-5>
           <v-card>
             <v-card-text>
               <v-form>
                 <v-text-field v-model="name" :counter="20" :rules="[v => !!v || '顧客名を入力してください']" label="顧客名" required ></v-text-field>
                 <v-text-field v-model="age" :counter="20"  label="年齢"></v-text-field>
                 <v-text-field v-model="sex" :counter="20" label="性別"></v-text-field>
-                <v-text-field v-model="memo" :counter="20" label="メモ"></v-text-field>
+                <v-text-field v-model="memo" :counter="20" label="メモ" height="150"></v-text-field>
                 <div class="text-center">
                   <v-btn @click="$router.push({ name: 'customers' })">キャンセル</v-btn>
                   <v-btn color="info" class="ml-2" @click="create">保存</v-btn>
@@ -51,7 +51,6 @@ export default {
         sex: this.sex,
         memo: this.memo 
       };
-      alert(params.name)
       this.axios
         .post(path, params)
       this.$router.push(
