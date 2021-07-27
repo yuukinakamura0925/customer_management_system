d9fbd2e840fa214e36a9db403180f26a6b378682
yuukinakamura0925 <ynakamara04@gmail.com>
2021-07-28 00:51:20 +0900

    Customers.vue 検索機能の編集、追加

--- front/src/views/Customers.vue
+++ front/src/views/Customers.vue
@@ -4,16 +4,17 @@
     <v-btn class="mt-16" @click="$router.push({ name: 'customers_new' })"
       >新規顧客登録</v-btn
     >
-    <div class="serch_box my-16">
-      <v-text-field
-        type="text"
-        v-model="keyword"
-        label="顧客名検索"
-      ></v-text-field>
-    </div>
+    
     <div class="my-16 ">
-      <v-container>
-        <v-data-table :headers="headers" :items="customers">
+      <v-container style="max-width: 900px">
+         <v-text-field
+           v-model="search"
+           append-icon="mdi-magnify"
+           label="顧客検索（ID、名前、性別、メモいずれかで検索）"
+           single-line
+           hide-details
+         ></v-text-field>
+        <v-data-table :headers="headers" :items="customers" :search="search">
           <template v-slot:[`item.action`]="{ item }">
             <router-link :to="{ name: 'customer', params: { id: item.id } }">
               <v-icon small class="mr-2">mdi-account-details</v-icon>
@@ -44,7 +45,7 @@ export default {
   components: {},
   data() {
     return {
-      keyword: "",
+      search: "",
       customers: [],
       page: 1,
       displayLists: [],
