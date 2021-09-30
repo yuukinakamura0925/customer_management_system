ec64a65f7297e8f29f4adaecffb220eb766e9d59
yuukinakamura0925 <ynakamara04@gmail.com>
2021-10-01 01:13:34 +0900

    cors.rb

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
+    origins ["http://backend.customer-management-system.link/","http://localhost:8080"]
 
     resource "*",
       headers: :any,
