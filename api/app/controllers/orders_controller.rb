38e14cb8c4ee09bc5e608eb89062f83caf8aeb28
yuukinakamura0925 <ynakamara04@gmail.com>
2021-10-07 23:42:29 +0900

    orders_controller

--- api/app/controllers/orders_controller.rb
+++ api/app/controllers/orders_controller.rb
@@ -1,40 +1,45 @@
 class OrdersController < ApplicationController
-def create
-    # カートを引っ張ってくる
-    cart = Cart.find(params[:cart_id])
-    customer_id = cart.customer_id
-    # カートディテールズ取得
-    cart_details = CartDetail.where(cart_id: cart.id)
-    # オーダーを登録する
-    order = Order.new
-    order.customer_id = customer_id
-    if order.save
-      render json: { status: "success", data: order }
-      # オーダーID取得
-      order_id = order.id
-    else
-      render json: { status: "error", data: order.errors }
-    end
-    order_details = []
-    cart_details.each do |cart_detail|
-      order_detail = OrderDetail.new 
-        
-      # オーダーディテールID取得
-      order_detail.order_id = order_id
-      order_detail.menu_price = cart_detail.price 
-      # order_detail.qty = cart_detail.qty
-      menus.each do |menu|
-        if menu.id == cart_detail.menu_id
-          order_detail.menu_name = menu.name
-        end
+  def create
+      # メニューを引っ張ってくる
+      menus = Menu.all
+      # カートを引っ張ってくる
+      cart = Cart.find(params[:cart_id])
+      customer_id = cart.customer_id
+      # カートディテールズ取得
+      cart_details = CartDetail.where(cart_id: cart.id)
+      # オーダーを登録する
+      order = Order.new
+      order.customer_id = customer_id
+      if order.save
+        render json: { status: "success", data: order }
+        # オーダーID取得
+        order_id = order.id
+      else
+        render json: { status: "error", data: order.errors }
       end
-        
-      order_details << order_detail
+      order_details = []
+      puts cart_details
+      puts "each文開始each文開始each文開始each文開始each文開始"
+      puts menus
+      cart_details.each do |cart_detail|
+        order_detail = OrderDetail.new 
+          
+        # オーダーディテールID取得
+        order_detail.order_id = order_id
+        order_detail.menu_price = cart_detail.price 
+        # order_detail.qty = cart_detail.qty
+        menus.each do |menu|
+          if menu.id == cart_detail.menu_id
+            order_detail.menu_name = menu.name
+          end
+        end
+          
+        order_details << order_detail
+      end
+      
+      OrderDetail.import order_details
+      cart_details.destroy_all 
     end
-    
-    OrderDetail.import order_details
-    cart_details.destroy_all 
-  end
 
   def index
     orders = Order.all
